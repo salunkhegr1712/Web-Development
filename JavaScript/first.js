32d6f1876e96af14fe13ccd4a70e4a7fe2d80907
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-06-10 09:37:10 +0530

    media query

---
+++ JavaScript/first.js
@@ -0,0 +1,6 @@
+// take a variable and take input with the use of the prompt and we ahev to store the value in the
+// varible you created
+
+var ghansham= prompt("What is ur name?");
+// then just alert the variable you created
+alert(ghansham)

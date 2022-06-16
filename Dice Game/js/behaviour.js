08a6aec12ebaee5492c021e21629cfa3dfa2a4b7
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-06-16 15:27:10 +0530

    Dice Game-updated

--- Dice Game/js/behaviour.js
+++ Dice Game/js/behaviour.js
@@ -1,3 +1,5 @@
+// getting javascript with varfibales and dom and targetting the img
+// so that we can change it according to the rfandom number
 var a=document.getElementsByTagName("img");
 
 var z=Math.random();
@@ -11,9 +13,14 @@ x=Math.floor(x*6)+1;
 var f="images/dice"+x+".png";
 a[1].setAttribute("src",f);
 
+// here we setting up for the printing new message accroding to who wins
+
 var d= document.firstElementChild.lastChild.firstElementChild.firstElementChild;
 
+// if else statement to print the message with imoji
 if(z > x){
+  // here we also can use textcontent but we dont have the emojis in JS
+  // so we have do the work with inner html
   d.innerHTML="<h1>&#128681; Player 1 wins</h1>";
 
 }
